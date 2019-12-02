48f498ba17c0c0cb17ffbc5b1a6329fcd2c5c84e
geuth <55501360+gth@users.noreply.github.com>
2019-12-02 11:20:39 -0600

    java

--- js/script.js
+++ js/script.js
@@ -1,24 +1,25 @@
-var slideIndex=0;
+var slideIndex = 0;
 showSlides(slideIndex);
 
 function plusSlides(n) {
     showSlides(slideIndex += n);
-  }
-function showSlides(){
+}
+
+function showSlides() {
     var i;
     var slides = document.getElementsByClassName("slideshow1");
     var dots = document.getElementsByClassName("dot");
-   
-    for (i=0; i<slides.length; i++){
-        slides[i].style.display="none";
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
     slideIndex++;
-    if (slideIndex>slides.length){slideIndex = 1}
-    for (i=0;i<dots.length; i++){
-        dots[i].className = dots[i].className.replace(" active", "")
+    if (slideIndex > slides.length) {
+        slideIndex = 1
     }
-   
-    slides[slideIndex-1].style.display="block";
-    dots[slideIndex-1].className += " active";
-    setTimeout(showSlides,5000);
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 2000);
 }
